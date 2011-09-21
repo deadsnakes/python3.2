af9d4c4ead29641003a92a40f5f38d8b87f4b91e
Felix Krull <f_krull@gmx.de>
2011-09-21 23:23:02 +0200

    Unapply all patches to prepare for merge.

--- Lib/tkinter/__init__.py
+++ Lib/tkinter/__init__.py
@@ -36,10 +36,7 @@ import sys
 if sys.platform == "win32":
     # Attempt to configure Tcl/Tk without requiring PATH
     from tkinter import _fix
-try:
-    import _tkinter
-except ImportError as msg:
-    raise ImportError(str(msg) + ', please install the python{}.{}-tk package'.format(*sys.version_info[:2]))
+import _tkinter # If this fails your Python may not be configured for Tk
 TclError = _tkinter.TclError
 from tkinter.constants import *
 
